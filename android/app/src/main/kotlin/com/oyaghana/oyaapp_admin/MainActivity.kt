48dd895f526fec4299a9f5a49c36cf225a762943
Precious Jahlom Agboado <jahlomp@Preciouss-MacBook-Pro.local>
2021-08-02 09:55:51 +0000

    updated Integrated ticket

--- android/app/src/main/kotlin/com/oyaghana/oyaapp_admin/MainActivity.kt
+++ android/app/src/main/kotlin/com/oyaghana/oyaapp_admin/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.oyaghana.oyaapp_admin
 
-
+import com.google.zxing.BarcodeFormat
 import android.util.Log
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
@@ -19,6 +19,7 @@ class MainActivity: FlutterActivity() {
     private var printThread: PrintThread? = null
     inner class PrintThread : Thread()
     {
+        var content = "1234567890"
         fun isThreadFinished(): Boolean
         {
             return mbThreadFinished
@@ -26,10 +27,20 @@ class MainActivity: FlutterActivity() {
 
         override fun run() {
             try {
-                posApiHelper.PrintInit(2, 24, 24, 0x33)
-                posApiHelper.PrintStr("   G.P.R.T.U \n\n")
-                posApiHelper.PrintStr("     Ticket \n\n")
-
+                posApiHelper.PrintBarcode(content, 240, 240, BarcodeFormat.QR_CODE)
+                posApiHelper.PrintSetFont(16.toByte(), 16.toByte(), 0x33.toByte())
+                posApiHelper.PrintStr("Ticket # : $content\n\n")
+                posApiHelper.PrintStr("------------------------\n")
+                posApiHelper.PrintSetFont(24.toByte(), 24.toByte(), 0x00.toByte())
+                posApiHelper.PrintStr(" OYA Test Print \n")
+                posApiHelper.PrintStr(" OYA Ticket \n")
+                posApiHelper.PrintStr(" Powered by OYA\n")
+                posApiHelper.PrintStr("\n")
+                posApiHelper.PrintStr("\n")
+                posApiHelper.PrintStr("\n")
+                posApiHelper.PrintStr("\n")
+                posApiHelper.PrintStr("\n")
+                posApiHelper.PrintStr("\n")
                 posApiHelper.PrintStart()
             } catch (ex: Exception) {
             }
