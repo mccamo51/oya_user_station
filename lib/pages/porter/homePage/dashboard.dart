036b6400845bf7a15041b0449e3cc6a6ab80f8a9
Bismark <mccamo51@gmail.com>
2021-08-19 14:03:32 +0000

    implemented metrix

--- lib/pages/porter/homePage/dashboard.dart
+++ lib/pages/porter/homePage/dashboard.dart
@@ -3,10 +3,13 @@ import 'dart:convert';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:intl/intl.dart';
 import 'package:oya_porter/components/appBar.dart';
 import 'package:oya_porter/config/routes.dart';
 import 'package:http/http.dart' as http;
 import 'package:oya_porter/pages/auth/login/login.dart';
+import 'package:oya_porter/spec/colors.dart';
 
 class DashBoard extends StatefulWidget {
   @override
@@ -15,13 +18,23 @@ class DashBoard extends StatefulWidget {
 
 class _DashBoardState extends State<DashBoard> {
   DateTime today = DateTime.now();
-
+  String passengers, trave, registered, frmDate, beginDte, fromRawDate;
+  bool isLoading = false;
   @override
   void initState() {
+    frmDate = DateFormat.MEd().format(today);
+    fromRawDate = "2021-07-19 10:48:19.621169";
+    beginDte = DateFormat.MEd().format(
+      DateTime.parse("2021-07-19 10:48:19.621169"),
+    );
     super.initState();
     _getData(
-      startDate: today.toUtc().toString(),
+      startDate: fromRawDate,
       endDate: today.toUtc().toString(),
+    );
+    _getTotal(
+      endDate: today.toUtc().toString(),
+      startDate: fromRawDate,
     );
   }
 
@@ -29,29 +42,141 @@ class _DashBoardState extends State<DashBoard> {
 
   @override
   Widget build(BuildContext context) {
-    print(today);
-
     return Scaffold(
         appBar: appBar(title: 'Metrix'),
         body: Column(children: [
-          //Initialize the chart widget
-          Container(
-            decoration: BoxDecoration(
-
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 15.0, right: 15, top: 5, bottom: 5),
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: WHITE,
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0.1, 0.7),
+                        color: Colors.grey.withOpacity(0.2),
+                        blurRadius: 0.5,
+                        spreadRadius: 0.3)
+                  ]),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Select Date (From - To)",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CupertinoButton(
+                          child: Text("$beginDte"),
+                          onPressed: () {
+                            _fromPickDate();
+                          }),
+                      Text("To"),
+                      CupertinoButton(
+                          child: Text("$frmDate"),
+                          onPressed: () {
+                            print("Time open");
+                            _pickDate();
+                          }),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-          
-          Row(
-            children: [
-              CupertinoButton(child: Text("Start Date"), onPressed: () {}),
-              Text("To"),
-              CupertinoButton(child: Text("End Date"), onPressed: () {}),
-            ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                cardContainer(
+                    icon: FeatherIcons.dollarSign,
+                    name: 'PASSENGERS',
+                    num: 2.3,
+                    color: Colors.green,
+                    context: context,
+                    number: isLoading ? 'Loading...' : '$passengers'),
+                cardContainer(
+                    icon: FeatherIcons.list,
+                    name: 'MINORS',
+                    num: 2.3,
+                    color: Colors.yellow[800],
+                    context: context,
+                    number: isLoading ? 'Loading...' : '$trave'),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: cardContainer(
+                icon: FeatherIcons.users,
+                name: 'REGISTERED USERS',
+                num: 1,
+                color: Colors.blue[700],
+                context: context,
+                number: isLoading ? 'Loading...' : '$registered'),
           )
         ]));
   }
 
-  _getData({String startDate, String endDate}) async {
+  _pickDate() async {
+    DateTime date = await showDatePicker(
+      context: context,
+      firstDate: DateTime(1980),
+      lastDate: DateTime(DateTime.now().year + 10),
+      initialDate: today,
+    );
+    if (date != null)
+      setState(() {
+        frmDate = DateFormat.MEd().format(date);
+        print("end Date ${date.toUtc().toString()}");
+        print("Start Date $fromRawDate");
+
+        _getData(
+          startDate: fromRawDate,
+          endDate: date.toUtc().toString(),
+        );
+        _getTotal(endDate: date.toUtc().toString(), startDate: fromRawDate);
+
+        // dateString =
+        //     "${date.year.toString()}-${date.month.toString()}-${date.day.toString()}";
+      });
+  }
+
+  _fromPickDate() async {
+    DateTime date = await showDatePicker(
+      context: context,
+      firstDate: DateTime(1980),
+      lastDate: DateTime(DateTime.now().year + 10),
+      initialDate: today,
+    );
+    if (date != null)
+      setState(() {
+        beginDte = DateFormat.MEd().format(date);
+        print("Start Date ${date.toUtc().toString()}");
+        fromRawDate = date.toUtc().toString();
+        // dateString =
+        //     "${date.year.toString()}-${date.month.toString()}-${date.day.toString()}";
+      });
+  }
+
+  Future<void> _getData({String startDate, String endDate}) async {
+    setState(() {
+      isLoading = true;
+    });
     try {
       final url = Uri.parse(
           "$BASE_URL/metrics/trips?paginate=true&limit=1&start_date=$startDate&end_date=$endDate");
@@ -63,9 +188,15 @@ class _DashBoardState extends State<DashBoard> {
           'Content-Type': 'application/json'
         },
       ).timeout(Duration(seconds: 50));
-      print(response.body);
       if (response.statusCode == 200) {
+        // setState(() {
+        //   isLoading = false;
+        // });
         final responseData = json.decode(response.body);
+        setState(() {
+          passengers = responseData['passengers'].toString();
+          trave = responseData['minors'].toString();
+        });
         _SalesData(
           'passengers',
           responseData['total'],
@@ -73,9 +204,50 @@ class _DashBoardState extends State<DashBoard> {
           responseData['passengers'],
         );
       } else {
+        setState(() {
+          isLoading = false;
+        });
         print("error");
       }
-    } catch (e) {}
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  _getTotal({String startDate, String endDate}) async {
+    try {
+      final url = Uri.parse(
+          "$BASE_URL/metrics/registrations?paginate=true&limit=1&start_date=$startDate&end_date=$endDate");
+
+      final response = await http.get(
+        url,
+        headers: {
+          "Authorization": "Bearer $accessToken",
+          'Content-Type': 'application/json'
+        },
+      ).timeout(Duration(seconds: 50));
+      print(response.body);
+      if (response.statusCode == 200) {
+        setState(() {
+          isLoading = false;
+        });
+        final responseData = json.decode(response.body);
+        setState(() {
+          registered = responseData['total'].toString();
+        });
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+        print("error");
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
 }
 
@@ -88,4 +260,75 @@ class _SalesData {
   final double passengers;
   final double total;
   Color colors;
+}
+
+// FeatherIcons.dollarSign
+Widget cardContainer(
+    {String name,
+    String number,
+    IconData icon,
+    double num = 1,
+    Color color,
+    BuildContext context}) {
+  return Stack(
+    children: [
+      Container(
+        width: MediaQuery.of(context).size.width / num,
+        height: 100,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: WHITE,
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(0.1, 0.7),
+                  color: Colors.grey.withOpacity(0.2),
+                  blurRadius: 0.5,
+                  spreadRadius: 0.3)
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "$name",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                    color: color,
+                  ),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text("$number",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ],
+            ),
+            Icon(icon)
+          ],
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 12.0),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+              height: 80,
+              width: 5,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      bottomLeft: Radius.circular(25)),
+                  color: color)),
+        ),
+      ),
+    ],
+  );
 }
